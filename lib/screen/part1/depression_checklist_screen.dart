7f38c7d4d97b550e67090bf9750def95fc58ec7b
dawon <dawon337@gmail.com>
2024-02-18 02:02:45 +0900

    design : depression_checklist_screen

--- lib/screen/part1/depression_checklist_screen.dart
+++ lib/screen/part1/depression_checklist_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smile_front/screen/part1/result_screen.dart';
+import '../../config/palette.dart'; // Palette 설정이 포함된 경로를 확인해주세요.
 
 class DepressionChecklistScreen extends StatefulWidget {
   const DepressionChecklistScreen({Key? key}) : super(key: key);
@@ -9,20 +10,17 @@ class DepressionChecklistScreen extends StatefulWidget {
 }
 
 class _DepressionChecklistScreenState extends State<DepressionChecklistScreen> {
-  // 체크리스트 데이터 - 예시
   final List<String> symptoms = [
     "일상 활동에 대한 관심 상실",
     "지속적인 우울한 기분",
     "수면 장애",
     "피로감 또는 에너지 상실",
-    // ... 기타 증상들
   ];
-  final List<bool> checked = []; // 체크 상태를 추적하는 리스트
+  final List<bool> checked = [];
 
   @override
   void initState() {
     super.initState();
-    // 모든 증상에 대해 체크 상태 초기화
     checked.addAll(List.generate(symptoms.length, (index) => false));
   }
 
@@ -30,27 +28,50 @@ class _DepressionChecklistScreenState extends State<DepressionChecklistScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('진단 체크리스트'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.white, // AppBar 아이콘 색상을 흰색으로 설정
+        ),
+        title: Image.asset(
+          'asset/img/smilelettering.png', // 앱바에 들어갈 이미지 경로
+          height: 40.0, // 이미지 높이 조절
+        ),
+        centerTitle: true,
       ),
+      backgroundColor: Palette.bgColor, // 배경색 설정
       body: ListView(
+        padding: const EdgeInsets.all(8.0),
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text('진단 체크리스트', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            child: Text('진단 체크리스트', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
           ),
-          for (int i = 0; i < symptoms.length; i++)
-            CheckboxListTile(
-              value: checked[i],
-              onChanged: (bool? value) {
-                setState(() {
-                  checked[i] = value!;
-                });
-              },
-              title: Text(symptoms[i]),
+          ...symptoms.map((symptom) => CheckboxListTile(
+            value: checked[symptoms.indexOf(symptom)],
+            onChanged: (bool? value) {
+              setState(() {
+                checked[symptoms.indexOf(symptom)] = value!;
+              });
+            },
+            title: Text(symptom, style: TextStyle(color: Colors.white)),
+            checkColor: Palette.bgColor, // 체크박스 내부 색상
+            activeColor: Colors.white, // 체크박스 외부 색상
+            controlAffinity: ListTileControlAffinity.leading, // 체크박스를 텍스트 앞으로 이동
+            side: BorderSide(color: Colors.white), // 체크박스 테두리 색상
+          ))
+              .toList(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100.0, vertical: 20.0),
+            child: ElevatedButton(
+              onPressed: _submitChecklist,
+              child: Text('제출하고 점수 확인하기', style: TextStyle(color: Colors.white)),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.transparent,
+                side: BorderSide(color: Colors.white, width: 2),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              ),
             ),
-          ElevatedButton(
-            onPressed: _submitChecklist,
-            child: Text('제출하고 점수 확인하기'),
           ),
         ],
       ),
@@ -58,10 +79,9 @@ class _DepressionChecklistScreenState extends State<DepressionChecklistScreen> {
   }
 
   void _submitChecklist() {
-    // 체크리스트 제출 로직
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => ResultScreen()), // 가상의 결과 스크린
+      MaterialPageRoute(builder: (context) => ResultScreen()),
     );
   }
 }
